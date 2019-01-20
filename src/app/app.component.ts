9753c87defae4e7fcbd7cf28f5d2c2b57f23437a
Bal Krishn Singh <balkrishn.singh@sojern.com>
2019-01-21 00:53:35 +0530

    setup package.json and node server for deploying angular app on heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'app';
+  ngOnInit() { this.servicecall(); }
+  servicecall() {
+      var request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+            console.log(request);
+          }
+        } else {
+          console.log(request);
+        }
+      };
+      var url = 'https://bksun.herokuapp.com/api/categories';
+      request.open("GET", url);
+      request.send();
+    };
 }
